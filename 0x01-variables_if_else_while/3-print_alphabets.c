4a48cd73b88739a8b3376a72321ff63cc4b292f0
USMAN <usmanumar_a@yahoo.co.uk>
2022-09-12 22:25:17 +0000

    My c project

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{		
+	int abc;
+
+		for (abc = 'a'; abc <= 'z'; abc++)
+		putchar(abc);
+
+		for (abc = 'A'; abc <= 'Z'; abc++)
+		putchar(abc);
+
+		putchar('\n');
+		return (0);
+}
