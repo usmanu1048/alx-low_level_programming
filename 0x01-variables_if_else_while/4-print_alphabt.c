7ccd16eacdcd84616f76b755859155afe0e33f6e
USMAN <usmanumar_a@yahoo.co.uk>
2022-09-13 00:54:40 +0000

    My C Project

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int abc;
+		for(abc = 'a'; abc <= 'z'; abc++)
+
+		putchar(abc);
+		putchar('\n');
+		
+		return(0);
+}		
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int cba;
+		for (cba = 'z'; cba >= 'a'; cba--)
+		putchar(cba);
+		putchar('\n');
+		return (0);
+}	
