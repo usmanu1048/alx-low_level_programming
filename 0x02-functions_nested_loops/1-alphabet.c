15135fd42c10d40a13d0e30c6e184570671301d8
USMAN <usmanumar_a@yahoo.co.uk>
2022-09-26 20:49:04 +0000

    My  project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * print_alphabet - Prints the alphabet
+  *
+  * Return: zero
+  */ 
+void print_alphabet(void)
+{
+	int g;
+	for(g = 'a';g < 'z';g++)
+	{	
+		_putchar(g);
+	}
+		_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 void print_putchar(void);
+void print_alphabet(void);
 
 #endif
