a327b5ee2cffe3801be508a4e4188c5bfc120f12
USMAN <usmanumar_a@yahoo.co.uk>
2022-09-26 21:15:06 +0000

    My C project

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - Prints alphabets ten times
+  *
+  * Return: zero
+  */
+void print_alphabet_x10(void)
+{
+	int d;
+
+	for (d = 'a'; d < 'z'; d++)
+	{
+		_putchar(d);
+	}
+		_putchar('\n');
+}
