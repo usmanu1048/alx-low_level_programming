c401bcce1cc109b6b7eb974968aa6ce03ae0a2ef
USMAN <usmanumar_a@yahoo.co.uk>
2022-09-26 20:55:37 +0000

    My C project

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,13 @@
   * print_alphabet - Prints the alphabet
   *
   * Return: zero
-  */ 
+  */
 void print_alphabet(void)
 {
 	int g;
-	for(g = 'a';g < 'z';g++)
-	{	
+
+	for (g = 'a'; g < 'z'; g++)
+	{
 		_putchar(g);
 	}
 		_putchar('\n');
