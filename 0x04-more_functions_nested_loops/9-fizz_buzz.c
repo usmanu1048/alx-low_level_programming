5f04e608508ad0789e1007e6be2b822ea93acf9e
USMAN <usmanumar_a@yahoo.co.uk>
2022-10-10 18:11:53 +0000

    My C project

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,21 @@ int main(void)
 	for (n = 2; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
-			{
-				printf(" FizzBuzz");
-													}
-			else if (n % 3 == 0)
-			{
-				printf(" Fizz");
-			}
-			else if (n % 5 == 0)
-			{
-				printf(" Buzz");
-			}
-			else
-			{
-				printf(" %d", n);
-			}
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
 	}
 	printf("\n");
 	return (0);
