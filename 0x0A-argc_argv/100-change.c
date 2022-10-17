4405807815806e1f93486ad5f66da36f8d17c1dd
USMAN <usmanumar_a@yahoo.co.uk>
2022-10-17 21:14:52 +0000

    My C project

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,7 +6,7 @@
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
-        */
+ */
 int main(int argc, char *argv[])
 {
 	int cents, ncoins = 0;
